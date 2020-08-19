fecfb25d914dd27faf14ee0c4af14a2d720b9266
rpolton <richaerd>
2020-08-19 14:49:10 +0100

    add preconditions

--- src/test/java/uk/co/qualitycode/utils/functional/Functional_Between_Test.java
+++ src/test/java/uk/co/qualitycode/utils/functional/Functional_Between_Test.java
@@ -1,25 +1,111 @@
 package uk.co.qualitycode.utils.functional;
 
+import io.vavr.Tuple2;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class Functional_Between_Test {
-    @Test
-    void betweenTest1() {
-        final int lowerBound = 2, upperBound = 4;
-        assertThat(Functional.between(lowerBound, upperBound, 3)).isTrue();
+    @Nested
+    class Preconditions {
+        @Test
+        void lowerBound() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(()->Functional.between(null, 4, 3))
+                    .withMessage("lower bound must not be null");
+        }
+
+        @Test
+        void upperBound() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(()->Functional.between(1, null, 3))
+                    .withMessage("upper bound must not be null");
+        }
+
+        @Test
+        void bounds() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(()->Functional.between(null, 3))
+                    .withMessage("bounds must not be null");
+        }
+
+        @Test
+        void value() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(()->Functional.between(new Tuple2<>(1,2), null))
+                    .withMessage("value must not be null");
+        }
+
+        @Test
+        void lowerBoundShouldBeLessThanUpperBound() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(()->Functional.between(2,1,3))
+                    .withMessage("lowerBound must be less than upperBound");
+        }
+    }
+    
+    @Nested
+    class Simple {
+        @Test
+        void betweenTest1() {
+            final int lowerBound = 2, upperBound = 4;
+            assertThat(Functional.between(lowerBound, upperBound, 3)).isTrue();
+        }
+
+        @Test
+        void betweenTest2() {
+            final int lowerBound = 2, upperBound = 4;
+            assertThat(Functional.between(lowerBound, upperBound, 1)).isFalse();
+        }
+
+        @Test
+        void betweenTest3() {
+            final double lowerBound = 2.5, upperBound = 2.6;
+            assertThat(Functional.between(lowerBound, upperBound, 2.55)).isTrue();
+        }
     }
 
-    @Test
-    void betweenTest2() {
-        final int lowerBound = 2, upperBound = 4;
-        assertThat(Functional.between(lowerBound, upperBound, 1)).isFalse();
+    @Nested
+    class Tupled {
+        @Test
+        void betweenTest1() {
+            final int lowerBound = 2, upperBound = 4;
+            assertThat(Functional.between(new Tuple2<>(lowerBound, upperBound), 3)).isTrue();
+        }
+
+        @Test
+        void betweenTest2() {
+            final int lowerBound = 2, upperBound = 4;
+            assertThat(Functional.between(new Tuple2<>(lowerBound, upperBound), 1)).isFalse();
+        }
+
+        @Test
+        void betweenTest3() {
+            final double lowerBound = 2.5, upperBound = 2.6;
+            assertThat(Functional.between(new Tuple2<>(lowerBound, upperBound), 2.55)).isTrue();
+        }
     }
 
-    @Test
-    void betweenTest3() {
-        final double lowerBound = 2.5, upperBound = 2.6;
-        assertThat(Functional.between(lowerBound, upperBound, 2.55)).isTrue();
+    @Nested
+    class Curried {
+        @Test
+        void betweenTest1() {
+            final int lowerBound = 2, upperBound = 4;
+            assertThat(Functional.between(new Tuple2<>(lowerBound, upperBound)).withValue( 3)).isTrue();
+        }
+
+        @Test
+        void betweenTest2() {
+            final int lowerBound = 2, upperBound = 4;
+            assertThat(Functional.between(new Tuple2<>(lowerBound, upperBound)).withValue( 1)).isFalse();
+        }
+
+        @Test
+        void betweenTest3() {
+            final double lowerBound = 2.5, upperBound = 2.6;
+            assertThat(Functional.between(new Tuple2<>(lowerBound, upperBound)).withValue( 2.55)).isTrue();
+        }
     }
 }
