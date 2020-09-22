89488272038451ba0778db776812d4a74cbd419b
rpolton <richaerd>
2020-09-22 12:37:11 +0100

    add unit test for constant and rename integration test to be more clear

--- src/test/java/uk/co/qualitycode/utils/functional/Functional_Constant_Test.java
+++ src/test/java/uk/co/qualitycode/utils/functional/Functional_Constant_Test.java
@@ -8,12 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Functional_Constant_Test {
     @Test
-    void constantInitialiserTest1() {
+    void constantShouldReturnTheSuppliedValue() {
+        final Object value = new Object();
+        assertThat(Functional.constant(value))
+                .satisfies(f -> assertThat(f.apply(2)).isSameAs(value));
+    }
+
+    @Test
+    void useConstantInInitAsAnInitialiser() {
         final int howMany = 6;
         final int initValue = -1;
         final Collection<Integer> l = Functional.init(Functional.constant(initValue), howMany);
-        assertThat(l).hasSize(howMany);
-        for (final int i : l)
-            assertThat(i).isEqualTo(initValue);
+        assertThat(l).hasSize(howMany).containsOnly(initValue);
     }
 }
